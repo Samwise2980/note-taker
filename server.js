8cae59c1893820046c37b0cfc8561c4c301d0cc1
Samwise <samuelfoster0@gmail.com>
2020-04-08 21:16:48 -0700

    created the path to the notes page on the server js.

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 app.listen(PORT, () => {
