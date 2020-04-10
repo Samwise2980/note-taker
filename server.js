0f3a7428979356786435723ecf990681d2e8a92a
Samwise <samuelfoster0@gmail.com>
2020-04-10 15:59:32 -0700

    you may now write notes and delete them.

--- server.js
+++ server.js
@@ -5,50 +5,79 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.static(path.join(__dirname, '/public/assets')));
-app.use(express.static(path.join(__dirname, '/public/assets/js')));
-app.use(express.static(path.join(__dirname, '/public/assets/css')));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/public/assets")));
+app.use(express.static(path.join(__dirname, "/public/assets/js")));
+app.use(express.static(path.join(__dirname, "/public/assets/css")));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-let holder = "";
+let holder = [];
+
+let lastID = 2;
 
 app.get("/api/notes", (req, res) => {
-  fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
+  fs.readFile("./db/db.json", (err, data) => {
     if (err) throw err;
-    let parsedData = JSON.parse(data)
-    holder = parsedData;
-    res.json(parsedData);
+    holder = JSON.parse(data);
+    return res.json(holder);
   });
 });
 
 app.post("/api/notes", (req, res) => {
-
   const newNote = req.body;
 
-  newNote["id"] = holder.length++;
-  newNote.append()
+  lastID += 1;
 
-  holder.push(newNote)
+  newNote["id"] = lastID;
 
-  const stringHolder = JSON.stringify(holder)
-  
-  fs.writeFileSync(path.join(__dirname, "/db/db.json"), stringHolder, "UTF8", function (err) {
-    if (err) return console.log(err);
-  });
-
-  fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
+  fs.readFile("./db/db.json", (err, data) => {
     if (err) throw err;
-    const parsedData = JSON.parse(data)
-    holder = parsedData;
-    res.json(parsedData);
+    holder = JSON.parse(data);
+
+    holder.push(newNote);
+
+    const stringHolder = JSON.stringify(holder);
+
+    fs.writeFile(
+      path.join(__dirname, "/db/db.json"),
+      stringHolder,
+      "UTF8",
+      function (err) {
+        if (err) return console.log(err);
+        return res.json(stringHolder);
+      }
+    );
   });
 });
 
 app.delete("/api/notes/:id", (req, res) => {
+  noteID = parseInt(req.params.id);
 
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) throw err;
+    holder = JSON.parse(data);
+
+    const idIndex = holder.findIndex((n) => n.id === noteID);
+
+    if (idIndex === -1) {
+      return res.sendStatus(404);
+    }
+
+    holder.splice(idIndex, 1);
+    const stringDB = JSON.stringify(holder);
+
+    fs.writeFile(
+      path.join(__dirname, "/db/db.json"),
+      stringDB,
+      "UTF8",
+      function (err) {
+        if (err) return console.log(err);
+        return res.sendStatus(200);
+      }
+    );
+  });
 });
 
 app.get("/notes", (req, res) => {
